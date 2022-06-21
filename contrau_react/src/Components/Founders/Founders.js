375d684d7e9fed455a186ad72d7499b300a247fc
DESKTOP-MOHOQ96\KRV <khoivudev@gmail.com>
2022-06-21 15:39:51 +0700

    add header, hero, group, founders and assets

---
+++ contrau_react/src/Components/Founders/Founders.js
@@ -0,0 +1,85 @@
+import React from "react";
+import male from "../../assets/image/male.png";
+import male2 from "../../assets/image/male2.png";
+import female from "../../assets/image/female.png";
+import MobileFounderCarousel from "./MobileFounderCarousel";
+export default function Founders() {
+  return (
+    <div className="ml-[36px]  lg:container  lg:mx-auto">
+      <div className="mt-[80px] md:mt-[300px]">
+        <div className="ml-0 lg:ml-[30px] 2xl:ml-0">
+          <p className="font-bold robotoFont  text-[13px]  md:text-[24px]">
+            Founders
+          </p>
+          <div className="hidden md:block leading-[1]]">
+            <p className="text-[50px] xl:text-[110px]  font-thin">
+              We don’t go for Deal,
+            </p>
+            <div className="flex items-center justify-start space-x-5 ">
+              <p className="text-[50px] xl:text-[110px] font-extrabold">
+                We go for{" "}
+              </p>
+              <div className="flex items-center justify-center">
+                <p className="text-[50px] xl:text-[110px] font-extrabold italic text-red">
+                  Founders
+                </p>
+                <div className="inline-block  bg-red w-[10px] h-[10px] xl:w-[23px] xl:h-[23px] rounded-full  mt-[30px] xl:mt-[54px] "></div>
+              </div>
+            </div>
+          </div>
+          <div className="block md:hidden leading-[1] mt-[27px] ">
+            <p className="italic text-[40px] font-thin ">We don't</p>
+            <p className="italic text-[40px] font-thin  ">go for Deals,</p>
+            <p className="font-extrabold italic text-[40px]">We go for</p>
+            <p className=" font-extrabold italic text-[40px] text-red">
+              Founders
+            </p>
+          </div>
+          <div className="text-[13px] mr-[80px] md:text-[20px] mt-[70px] ">
+            <p>
+              Our KPI is not the company's follow-up investment attraction
+              ratio. Our KPI is the rate at which founders,
+            </p>
+            <p>
+              who have already invested in the company, come to us first to
+              receive seed investment. We want to be the first place founders
+            </p>
+            <p>
+              call in the most desperate situations. Vietnam's victory is our
+              victory.
+            </p>
+          </div>
+
+          <div className=" mt-[78px] lg:mt-[200px]">
+            <div className=" hidden md:grid grid-cols-3 gap-x-4 2xl:mx-[300px]">
+              <div>
+                <img
+                  src={male}
+                  alt="male"
+                  className="w-[278px] h-[431px] rounded-3xl"
+                />
+              </div>
+              <div>
+                <img
+                  src={male2}
+                  alt="male2"
+                  className="w-[278px] h-[431px] rounded-3xl"
+                />
+              </div>
+              <div>
+                <img
+                  src={female}
+                  alt="female"
+                  className="w-[278px] h-[431px] rounded-3xl"
+                />
+              </div>
+            </div>
+            <div className="md:hidden ml-[25px] overflow-hidden ">
+              <MobileFounderCarousel />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- contrau_react/tailwind.config.js
+++ contrau_react/tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#101010",
+      red: "#DB2F33",
+      grey: "#F9F9F9",
+      hoverGrey: "#828383",
+    },
   },
   plugins: [],
-}
+};
