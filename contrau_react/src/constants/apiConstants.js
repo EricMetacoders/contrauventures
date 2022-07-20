809a15b59c69ec269fa6719d4712f2b3daa43b94
Tom <tom@welinc.vn>
2022-07-20 10:27:48 +0700

    Favicon

--- contrau_react/src/constants/apiConstants.js
+++ contrau_react/src/constants/apiConstants.js
@@ -1,4 +1,4 @@
-//export const BASE_URL = "https://contrau.metacoders.dev/wp-json/acf/v3/";
-//export const BASE_URL_CONTACT = "https://contrau.metacoders.dev/wp-json/contact-form-7/v1/contact-forms/325/feedback";
-export const BASE_URL = "https://contrau.ventures/wp-json/acf/v3/";
-export const BASE_URL_CONTACT = "https://contrau.ventures/wp-json/contact-form-7/v1/contact-forms/325/feedback";
+export const BASE_URL = "https://contrau.metacoders.dev/wp-json/acf/v3/";
+export const BASE_URL_CONTACT = "https://contrau.metacoders.dev/wp-json/contact-form-7/v1/contact-forms/325/feedback";
+//export const BASE_URL = "https://contrau.ventures/wp-json/acf/v3/";
+//export const BASE_URL_CONTACT = "https://contrau.ventures/wp-json/contact-form-7/v1/contact-forms/325/feedback";
