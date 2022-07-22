10bf514b627af4e5f989ecd61c154c53c5fda05a
hoang le <hoang.le@metacoders.dev>
2022-07-22 14:55:14 +0700

    refactor PartnerLogo component

--- contrau_react/src/Pages/Home/Components/Group/partnerLogo/partnerLogo.jsx
+++ contrau_react/src/Pages/Home/Components/Group/partnerLogo/partnerLogo.jsx
@@ -4,31 +4,18 @@ export const PartnerLogo = ({ logoUrl, logoName, wrapperWidth, wrapperHeight, pa
   return (
     <Wrapper>
       <div className="pl-[90px]">
-        {
-          linkWebsite
-          ?
-          <LinkLogo
-            {...{ wrapperWidth, wrapperHeight, paddingLeft }}
-            href={linkWebsite}
-            target={linkWebsite}
-          >
-            <BackgroundLogo >
-              <Logo
-                src={logoUrl}
-                alt={logoName}
-              />
-            </BackgroundLogo>
-          </LinkLogo>
-          :
-          <LinkLogo {...{ wrapperWidth, wrapperHeight, paddingLeft }}>
-            <BackgroundLogo >
-              <Logo
-                src={logoUrl}
-                alt={logoName}
-              />
-            </BackgroundLogo>
-          </LinkLogo>
-        }
+        <LinkLogo
+          {...{ wrapperWidth, wrapperHeight, paddingLeft }}
+          href={linkWebsite || undefined}
+          target={linkWebsite ? "_blank" : undefined}
+        >
+          <BackgroundLogo >
+            <Logo
+              src={logoUrl}
+              alt={logoName}
+            />
+          </BackgroundLogo>
+        </LinkLogo>
       </div>
     </Wrapper>
   )
