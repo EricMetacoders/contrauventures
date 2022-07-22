ef0e5b879e65ed32bd03a69fb15cd2ea072c2444
hoang le <hoang.le@metacoders.dev>
2022-07-22 14:38:57 +0700

    delete delect link to homepage when click red dot logo

--- contrau_react/src/Pages/Home/Components/Group/partnerLogo/partnerLogo.jsx
+++ contrau_react/src/Pages/Home/Components/Group/partnerLogo/partnerLogo.jsx
@@ -1,17 +1,34 @@
-import { Logo, Wrapper, BackgroundLogo, LinkLogo } from "./PartnerLogoStyled";
+import { Logo, Wrapper, BackgroundLogo, LinkLogo } from "./partnerLogoStyled";
 
 export const PartnerLogo = ({ logoUrl, logoName, wrapperWidth, wrapperHeight, paddingLeft, linkWebsite }) => {
   return (
     <Wrapper>
       <div className="pl-[90px]">
-        <LinkLogo {...{ wrapperWidth, wrapperHeight, paddingLeft }} href={linkWebsite} target="_blank">
-          <BackgroundLogo >
-            <Logo
-              src={logoUrl}
-              alt={logoName}
-            />
-          </BackgroundLogo>
-        </LinkLogo>
+        {
+          linkWebsite
+          ?
+          <LinkLogo
+            {...{ wrapperWidth, wrapperHeight, paddingLeft }}
+            href={linkWebsite}
+            target={linkWebsite}
+          >
+            <BackgroundLogo >
+              <Logo
+                src={logoUrl}
+                alt={logoName}
+              />
+            </BackgroundLogo>
+          </LinkLogo>
+          :
+          <LinkLogo {...{ wrapperWidth, wrapperHeight, paddingLeft }}>
+            <BackgroundLogo >
+              <Logo
+                src={logoUrl}
+                alt={logoName}
+              />
+            </BackgroundLogo>
+          </LinkLogo>
+        }
       </div>
     </Wrapper>
   )
