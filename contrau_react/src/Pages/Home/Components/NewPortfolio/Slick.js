2188b4acc1fcbbafde9fec2f818fd6fe7f169b90
DESKTOP-MOHOQ96\KRV <khoivudev@gmail.com>
2022-07-25 15:24:25 +0700

    refactor Porfolio

--- contrau_react/src/Pages/Home/Components/NewPortfolio/Slick.js
+++ contrau_react/src/Pages/Home/Components/NewPortfolio/Slick.js
@@ -1,12 +1,14 @@
 import Carousel from "nuka-carousel";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 import "./slick.scss";
 
 export default function Slick() {
+  // get data from HomeSlice
   const viewAll = useSelector((state) => state.homeSlice.portfolios);
-  // set data
+
+  // Filter Data
   const digitalSupplyChain = viewAll?.filter((logo) => {
     return logo.acf.category === "Digital Supply Chain";
   });
@@ -16,13 +18,28 @@ export default function Slick() {
   const sWInfrastructure = viewAll?.filter((logo) => {
     return logo.acf.category === "SW Infrastructure";
   });
-  // set show div
-  const [showViewAll, setShowViewAll] = useState(true);
-  const [showDigitalSupplyChain, setShowDigitalSupplyChain] = useState(false);
-  const [showSustainableMegacity, setShowSustainableMegacity] = useState(false);
-  const [showSWInfrastructure, setShowSWInfrastructure] = useState(false);
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setData(viewAll);
+  }, [viewAll]);
+
+  // set Data
+  const handleSetData = (name) => {
+    if (name === "View All") {
+      setData(viewAll);
+    } else if (name === "Digital Supply Chain") {
+      setData(digitalSupplyChain);
+    } else if (name === "Sustainable Megacity") {
+      setData(sustainableMegacity);
+    } else if (name === "SW Infrastructure") {
+      setData(sWInfrastructure);
+    }
+  };
 
   // handle active class
+
   const [viewAllActive, setViewAllActive] = useState(true);
   const [digitalSupplyChainActive, setDigitalSupplyChainActive] =
     useState(false);
@@ -30,59 +47,28 @@ export default function Slick() {
     useState(false);
   const [sWInfrastructureActive, setSWInfrastructureActive] = useState(false);
 
-  // handle show div
-  const handleShowViewAll = () => {
-    setShowViewAll(true);
-    setShowDigitalSupplyChain(false);
-    setShowSustainableMegacity(false);
-    setShowSWInfrastructure(false);
-  };
-  const handleShowDigitalSupplyChain = () => {
-    setShowDigitalSupplyChain(true);
-    setShowViewAll(false);
-    setShowSustainableMegacity(false);
-    setShowSWInfrastructure(false);
-  };
-  const handleShowSustainableMegacity = () => {
-    setShowSustainableMegacity(true);
-    setShowDigitalSupplyChain(false);
-    setShowViewAll(false);
-    setShowSWInfrastructure(false);
-  };
-  const handleShowSWInfrastructure = () => {
-    setShowSWInfrastructure(true);
-    setShowSustainableMegacity(false);
-    setShowDigitalSupplyChain(false);
-    setShowViewAll(false);
-  };
-
-  // handle Active class
-  const handleViewAllActive = () => {
-    setViewAllActive(true);
-    setDigitalSupplyChainActive(false);
-    setSustainableMegacityActive(false);
-    setSWInfrastructureActive(false);
-  };
-
-  const handleSustainableMegacityActive = () => {
-    setSustainableMegacityActive(true);
-    setDigitalSupplyChainActive(false);
-    setViewAllActive(false);
-    setSWInfrastructureActive(false);
-  };
-
-  const handleDigitalSupplyChainActive = () => {
-    setDigitalSupplyChainActive(true);
-    setViewAllActive(false);
-    setSustainableMegacityActive(false);
-    setSWInfrastructureActive(false);
-  };
-
-  const handleSWInfrastructureActive = () => {
-    setSWInfrastructureActive(true);
-    setDigitalSupplyChainActive(false);
-    setViewAllActive(false);
-    setSustainableMegacityActive(false);
+  const handleActiveClass = (name) => {
+    if (name === "View All") {
+      setViewAllActive(true);
+      setDigitalSupplyChainActive(false);
+      setSustainableMegacityActive(false);
+      setSWInfrastructureActive(false);
+    } else if (name === "Digital Supply Chain") {
+      setDigitalSupplyChainActive(true);
+      setViewAllActive(false);
+      setSustainableMegacityActive(false);
+      setSWInfrastructureActive(false);
+    } else if (name === "Sustainable Megacity") {
+      setSustainableMegacityActive(true);
+      setDigitalSupplyChainActive(false);
+      setViewAllActive(false);
+      setSWInfrastructureActive(false);
+    } else if (name === "SW Infrastructure") {
+      setSWInfrastructureActive(true);
+      setDigitalSupplyChainActive(false);
+      setViewAllActive(false);
+      setSustainableMegacityActive(false);
+    }
   };
 
   return (
@@ -92,8 +78,8 @@ export default function Slick() {
         <div
           className="h-[56px]"
           onClick={() => {
-            handleShowViewAll();
-            handleViewAllActive();
+            handleSetData("View All");
+            handleActiveClass("View All");
           }}
         >
           <div className={viewAllActive ? "active" : "notActive"}>
@@ -109,8 +95,9 @@ export default function Slick() {
         <div
           className=" h-[56px]"
           onClick={() => {
-            handleShowSWInfrastructure();
-            handleSWInfrastructureActive();
+            // handleShowSWInfrastructure();
+            handleSetData("SW Infrastructure");
+            handleActiveClass("SW Infrastructure");
           }}
         >
           <div className={sWInfrastructureActive ? "active" : "notActive"}>
@@ -126,8 +113,9 @@ export default function Slick() {
         <div
           className="h-[56px] "
           onClick={() => {
-            handleShowDigitalSupplyChain();
-            handleDigitalSupplyChainActive();
+            // handleShowDigitalSupplyChain();
+            handleSetData("Digital Supply Chain");
+            handleActiveClass("Digital Supply Chain");
           }}
         >
           <div className={digitalSupplyChainActive ? "active" : "notActive"}>
@@ -143,8 +131,9 @@ export default function Slick() {
         <div
           className="h-[56px]"
           onClick={() => {
-            handleShowSustainableMegacity();
-            handleSustainableMegacityActive();
+            // handleShowSustainableMegacity();
+            handleSetData("Sustainable Megacity");
+            handleActiveClass("Sustainable Megacity");
           }}
         >
           <div className={sustainableMegacityActive ? "active" : "notActive"}>
@@ -157,87 +146,9 @@ export default function Slick() {
         </div>
       </Carousel>
       {/* showAll data */}
-      {showViewAll ? (
+      {data ? (
         <div className="flex flex-wrap col gap-x-[10px] gap-y-[20px] justify-left mt-[26px] mx-auto w-[300px]">
-          {viewAll?.map((logo, i) => {
-            return (
-              <>
-                <a href={logo.acf.linkWebsite} target="_blank">
-                  <div key={i}>
-                    <div className="flex items-center justify-center">
-                      <img
-                        src={logo?.acf?.image}
-                        alt={logo?.acf?.category}
-                        className="opacity-80 hover:opacity-100 w-[63px] h-[36px] object-contain"
-                      />
-                    </div>
-                  </div>
-                </a>
-                {(i + 1) % 4 === 0 ? <hr className="w-full bg-white" /> : <></>}
-              </>
-            );
-          })}
-        </div>
-      ) : (
-        <></>
-      )}
-
-      {/* SWInfrastructure data */}
-      {showSWInfrastructure ? (
-        <div className="flex flex-wrap col gap-x-[10px]  gap-y-[20px]   justify-left mt-[26px] mx-auto w-[300px]">
-          {sWInfrastructure?.map((logo, i) => {
-            return (
-              <>
-                <a href={logo.acf.linkWebsite} target="_blank">
-                  <div key={i}>
-                    <div className="flex items-center justify-center">
-                      <img
-                        src={logo?.acf?.image}
-                        alt={logo?.acf?.category}
-                        className="opacity-80 hover:opacity-100 w-[63px] h-[36px] object-contain"
-                      />
-                    </div>
-                  </div>
-                </a>
-                {(i + 1) % 4 === 0 ? <hr className="w-full bg-white" /> : <></>}
-              </>
-            );
-          })}
-        </div>
-      ) : (
-        <></>
-      )}
-
-      {/* DigitalSupplyChain data */}
-      {showDigitalSupplyChain ? (
-        <div className="flex flex-wrap col gap-x-[10px]  gap-y-[20px]   justify-left mt-[26px] mx-auto w-[300px]">
-          {digitalSupplyChain?.map((logo, i) => {
-            return (
-              <>
-                <a href={logo.acf.linkWebsite} target="_blank">
-                  <div key={i}>
-                    <div className="flex items-center justify-center">
-                      <img
-                        src={logo?.acf?.image}
-                        alt={logo?.acf?.category}
-                        className="opacity-80 hover:opacity-100 w-[63px] h-[36px] object-contain"
-                      />
-                    </div>
-                  </div>
-                </a>
-                {(i + 1) % 4 === 0 ? <hr className="w-full bg-white" /> : <></>}
-              </>
-            );
-          })}
-        </div>
-      ) : (
-        <></>
-      )}
-
-      {/* SustainableMegacity data */}
-      {showSustainableMegacity ? (
-        <div className="flex flex-wrap col gap-x-[10px]  gap-y-[20px]   justify-left mt-[26px] mx-auto w-[300px]">
-          {sustainableMegacity?.map((logo, i) => {
+          {data?.map((logo, i) => {
             return (
               <>
                 <a href={logo.acf.linkWebsite} target="_blank">
