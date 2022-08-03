9fa73344102618503b4662299b09a43781d39cd1
DESKTOP-MOHOQ96\KRV <khoivudev@gmail.com>
2022-08-03 10:26:02 +0700

    fix layout contact and footer of homepage

--- contrau_react/src/Pages/Home/Components/Contact/Contact.js
+++ contrau_react/src/Pages/Home/Components/Contact/Contact.js
@@ -6,7 +6,7 @@ export default function Contact() {
     <div className="mt-[142px] lg:mt-[243px]">
       <div className=" mx-[12px] md:mx-[40px] xl:mx-[40px] lg:mx-[31px] 2xl:mx-[181px] ">
         <div className="bg-partnersContactBg pb-[32px] 2xl:pb-[115px] shadowContact">
-          <div className="pt-[16%] md:pt-[64px] md:mx-[76px] lg:pt-[124px] mx-[24px] lg:mx-[88px] xl:mx-[123px]">
+          <div className="pt-[16%] md:pt-[64px] md:mx-[76px] lg:pt-[124px] mx-[24px] lg:mx-[88px] xl:mx-[62px] 2xl:mx-[123px]">
             <div className="flex items-center">
               <p className="popinsFont italic text-[40px]  text-white lg:text-[60px] font-[900]">
                 Contact{" "}
